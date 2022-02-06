552061b5243b7424b6ef3e774afe65aebf880889
GeeinKim <gkim13@risd.edu>
2022-02-06 14:51:33 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function(){
+
+    
+    
+});
+
+    // $(".row-2").children().draggable();
+
+    // var counter = 0;
+    // $(".row-2").children().click(function(){
+    //     console.log("click")
+    //     counter = counter + 1;
+    //     $(this).css('z-index', counter);
+    // });
+ 
+
+
+
